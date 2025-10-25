61c0e1af4f5cc43e0d18070700aefdd8f6ecb1b3
Rahul Walse <rahul_walse@hotmail.com>
2025-10-25 13:21:55 +0530

    Programming Exercise 1 from Section 2.2.

---
+++ Chapter 2 - Overview of C/section2-2ex1.c
@@ -0,0 +1,15 @@
+/*
+    Write the #define preprocessor directive and declarations for a program
+    that has a constant macro for PI (3.14159) and variables radius, area, and
+    circumf declared as double, variable num_circ as an int, and variable
+    circ_name as a char.
+*/
+
+#define PI 3.14159
+
+double area;
+double circumf;
+double radius;
+
+int num_circ;
+char circ_name;
